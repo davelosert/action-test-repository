d82dbfd75ffa79feecad40ff117fc7068e29dad3
David Losert <davelosert@github.com>
2023-06-13 10:40:47 +0000

    test with istanbul coverage

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      provider: 'istanbul',
       reporter: ['text', 'json', 'json-summary'],
     },
   },
