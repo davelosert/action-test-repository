c31c84b93be7ca05c246db10c05f6cbfe4dbfa87
David Losert <davelosert@github.com>
2024-08-09 13:54:49 +0200

    Update file.test.ts

--- src/file.test.ts
+++ src/file.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { sum, sub } from './file';
 
-// Add something
+// Add something more
 describe('file', () => {
   it('sum 1+1 = 2', () => {
     expect(sum(1, 1)).toBe(2);
